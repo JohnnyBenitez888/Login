713c3c19b769767b52f1adcf64f74dd02644d34b
Johnny Benitez <johnnybeniitez@gmail.com>
2024-02-15 10:46:15 -0300

    Clase Usuario, Persistence Unit, JFrame Principal
    Conexion a BD login.
    Mapeo de la clase Usuario.
    Driver mysql connector.

--- Login/src/main/java/com/todocode/ap/login/logica/Usuario.java
+++ Login/src/main/java/com/todocode/ap/login/logica/Usuario.java
@@ -1,9 +1,65 @@
 package com.todocode.ap.login.logica;
 
-/**
- *
- * @author Johnny
- */
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario {
+    //Atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private int id;
+    private String nombreUsuario;
+    private String contrasenia;
+
+    //Constructores
+    public Usuario() {
+    }
+
+    public Usuario(String nombreUsuario, String contrasenia) {
+        this.nombreUsuario = nombreUsuario;
+        this.contrasenia = contrasenia;
+    }
+
+    public Usuario(int id, String nombreUsuario, String contrasenia) {
+        this.id = id;
+        this.nombreUsuario = nombreUsuario;
+        this.contrasenia = contrasenia;
+    }
+
+    //Getter and Setter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    //ToString
+    @Override
+    public String toString() {
+        return "Usuario{" + "id=" + id + ", nombreUsuario=" + nombreUsuario + ", contrasenia=" + contrasenia + '}';
+    }
+    
+    
     
 }
